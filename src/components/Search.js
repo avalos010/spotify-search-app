f82051e05a55955927d5e64123f58084ed12f3f3
avalos010 <luizavalos40@gmail.com>
2018-02-07 19:34:31 -0600

    Initial Commit

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+let Search = (props) => (
+    <div>
+           <form onSubmit={e => props.search(e)} className="form-input"> 
+          <input type="text" placeholder="Track Name" />
+          <input type="submit" value="search" />
+           </form>
+        </div>
+)
+
+export default Search;
