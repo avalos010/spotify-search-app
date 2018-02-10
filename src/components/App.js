bc99f03854428a3c50653b53d7f1d852767f4272
JLuis Avalos <luizavalos40@gmail.com>
2018-02-10 17:31:18 -0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ class App extends Component {
 search = (e) => {
     e.preventDefault();
     this.setState({term: e.target[0].value})
-    fetch('http://localhost:3050/search', {
+    fetch('https://spotify-backend-api.herokuapp.com/search', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
