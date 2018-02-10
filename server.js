1d51a51493d11c78cd2281c92a1fceee663e62f0
JLuis Avalos <luizavalos40@gmail.com>
2018-02-10 17:16:47 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const Spotify = require('node-spotify-api');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+let app = express();
+let spotify = new Spotify({
+    id: '9dbe35ea2e34460788c9594ccb1cd22c',
+    secret: 'ad08907f3ec440c6bc6376968f8b599b'
+})
+
+app.use(bodyParser());
+app.use(cors());
+
+app.get('/',(req,res) => {
+    res.send('Server up and Running');
+});
+
+
+app.post('/search', (req,res) => {
+    req.body && spotify.search({type: 'track', query: req.body.term})
+    .then(data => res.json(data.tracks.items))
+});
+
+app.listen(3050, () => {
+    console.log('port up and running');
+})
