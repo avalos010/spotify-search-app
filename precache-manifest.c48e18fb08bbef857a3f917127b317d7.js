3592784146a9f38564cfa6bcc84d51e6c9122093
avalos010 <luizavalos40@gmail.com>
2019-05-15 19:10:29 -0500

    Updates

---
+++ precache-manifest.c48e18fb08bbef857a3f917127b317d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa0c274a58cbed5d10b5f93d5f6d98a",
+    "url": "/spotify-search-app/index.html"
+  },
+  {
+    "revision": "aff2891fb2dd0fce6336",
+    "url": "/spotify-search-app/static/css/main.fcaea21b.chunk.css"
+  },
+  {
+    "revision": "6323ea5fe1cd3cc7d471",
+    "url": "/spotify-search-app/static/js/2.14a2000e.chunk.js"
+  },
+  {
+    "revision": "aff2891fb2dd0fce6336",
+    "url": "/spotify-search-app/static/js/main.37dc4f75.chunk.js"
+  },
+  {
+    "revision": "acacb784aa64aca7a358",
+    "url": "/spotify-search-app/static/js/runtime~main.e84345f2.js"
+  },
+  {
+    "revision": "d46173d019278f0871346319ead76357",
+    "url": "/spotify-search-app/static/media/background.d46173d0.jpg"
+  }
+]);
